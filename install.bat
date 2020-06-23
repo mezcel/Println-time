42461c92001b45c3b40718a610bf72984cdbe650
mezcel <mezcel@hotmail.com>
2020-06-23 08:47:58 -0400

    added redundant installer scripts

---
+++ install.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+REM Install struct-fmt
+go get -u github.com/mezcel/struct-fmt
+
+REM Install andlabs/ui
+go get -u github.com/andlabs/ui
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+## Install struct-fmt
+go get github.com/mezcel/struct-fmt
+
+## Install andlabs/ui
+go get github.com/andlabs/ui
