b735cdfb722179177fcca98e8efd5b3ec959ddac
mezcel <mezcel@hotmail.com>
2020-06-25 11:20:24 -0400

    updated comments

--- install.bat
+++ install.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 
 REM Install struct-fmt
+ECHO Installing github.com/mezcel/struct-fmt ...
 go get -u github.com/mezcel/struct-fmt
 
 REM Install andlabs/ui
+ECHO Installing github.com/andlabs/ui ...
 go get -u github.com/andlabs/ui
--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ## Install struct-fmt
+echo "Installing github.com/mezcel/struct-fmt ... "
 go get github.com/mezcel/struct-fmt
 
 ## Install andlabs/ui
+echo "Installing github.com/andlabs/ui ... "
 go get github.com/andlabs/ui
