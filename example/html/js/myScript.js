39d1d093af7b42c1d74860b64cc028bd73a16d35
mezcel <mezcel@hotmail.com>
2020-07-19 18:57:31 -0400

    update dom

--- example/html/js/myScript.js
+++ example/html/js/myScript.js
@@ -1,13 +1,47 @@
+/* myScript.js */
+
+// Toggle w3css dropdown menu display
+function toggleDropdown() {
+    var x = document.getElementById("Menu");
+
+    if (x.className.indexOf("w3-show") == -1) { 
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
 // Button Key bindings
 document.onkeyup = function(e){
-    switch(e.which) {
+    var keyPress = e.which;
+    switch(keyPress) {
         case 39:        // rt arrow
         case 76:        // l
-            document.getElementById( "next" ).click()
+            // navigate to next position
+            var btnNext = document.getElementById( 'btnNext' );
+
+            if ( btnNext ) {
+                btnNext.click();
+            }
             break;
         case 72:        // h
         case 37:        // lt arrow
-            document.getElementById( "back" ).click()
+            // navigate to previous position
+            var btnBack = document.getElementById( 'btnBack' );
+            var btnIndex = document.getElementById( 'btnIndex' );
+
+            if ( btnBack ) {
+                btnBack.click();
+            }
+            if ( btnIndex ) {
+                btnIndex.click();
+            }
             break;
+        case 32:        // spacebar
+            // toggle dropdown mwnu
+            toggleDropdown();
+            break;
+        default:
+            console.log(keyPress);
     }
 }
