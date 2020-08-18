38e41f6113616dc37082456196787ad32c222629
mezcel <mezcel@hotmail.com>
2020-08-18 14:49:38 -0400

    update godoc readme

--- example/doc.go
+++ example/doc.go
@@ -12,14 +12,16 @@ About
 
 Demo scripts
 
-## launch one of the following Go scripts
-go run main.go          ## Cli/Tui Rosary with mezcel/struct-fmt package
+## Cli/Tui Rosary with mezcel/struct-fmt package
+```go run main.go```
 * This is a CLI demonstration on using the json to struct feature, and basic navigation and initialization functions.
 
-go run main-ui.go       ## Gui Rosary with mezcel/struct-fmt & andlabs/ui package
+## Gui Rosary with mezcel/struct-fmt & andlabs/ui package
+```go run main-ui.go```
 * This is a demo example using an OS's native desktop environment UI client.
 
-go run main-dom.go      ## Go Webserver web page
+## Go Webserver web page
+```go run main-dom.go```
 * This is a demo using a web server to host a web browser web page user interface. This website also features web scraping.
 
 */
