32ae5b2811d973eedd538dcbd8e8f8f833069364
mezcel <mezcel@hotmail.com>
2020-08-18 14:43:58 -0400

    update godoc readme

--- example/doc.go
+++ example/doc.go
@@ -1,51 +1,26 @@
 /*
-# Example (use case demos)
+Example (use case demos)
 
-Use case demos using the ```struct-fmt``` package.
 
-## About
+> Use case demos using the ```struct-fmt``` package.
+
+
+About
 
 * Demonstration apps which use the struct-fmt Golang package.
 
----
 
-## Demos
-
-### Prerequisite dependencies
-
-```sh
-
-## Navigate into the repo's demo directory
-cd example/
-
-## Install prerequisites
-./install-example.sh
-```
-
-### Launch runtime
-
-```sh
-## Navigate into the repo's demo directory
-cd example/
+Demo scripts
 
 ## launch one of the following Go scripts
 go run main.go          ## Cli/Tui Rosary with mezcel/struct-fmt package
+* This is a CLI demonstration on using the json to struct feature, and basic navigation and initialization functions.
+
 go run main-ui.go       ## Gui Rosary with mezcel/struct-fmt & andlabs/ui package
+* This is a demo example using an OS's native desktop environment UI client.
+
 go run main-dom.go      ## Go Webserver web page
-```
----
-
-## Screenshots ( Outdated )
-
-Gif screenshots and videos were taken earlier in the development time line. Small modifications have continued to be added.
-
-| main-ui.go (win10) | main-ui.go (GTK) | main-dom.go (Web Browser) [YouTube](https://www.youtube.com/watch?v=diSGGO_kDZ0) |
-|:---:|:---:|:---:|
-|![win10.gif](./screenshots/win10.gif)|![wsl-gtk.gif](./screenshots/wsl-gtk.gif)|![chrome.gif](screenshots/chrome.gif)|
-
-| main.go (CLI Terminal) |
-|:---:|
-|[![asciicast](https://asciinema.org/a/343751.svg)](https://asciinema.org/a/343751)|
+* This is a demo using a web server to host a web browser web page user interface. This website also features web scraping.
 
 */
 package main
